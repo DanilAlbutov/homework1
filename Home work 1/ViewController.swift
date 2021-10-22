335a50c017d235aaab1448c1aa3c18a147b48238
Danil Albutov <albutovdanil@gmail.com>
2021-10-22 21:02:04 +0400

    task complited

--- Home work 1/ViewController.swift
+++ Home work 1/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet weak var label: UILabel!
+    
+    private var counter: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func onButtonClick(_ sender: UIButton) {
+        counter = getIntFromLabel()
+        switch sender.tag{
+            case 2: // +
+                counter += 1
+            case 3: // clear
+                counter = 0
+            case 4: // -
+                counter -= 1
+            default:
+                counter = 0
+        }
+        label.text = String(counter)
+        checkBorder()
+    }
+    
+    func getIntFromLabel() -> Int {
+        return Int(label.text!) ?? 0
+    }
+    
+    func callAllert() {
+        let alert = UIAlertController(title: "Уведомление", message: "Превышен лимит [-10; 10]", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+        NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func checkBorder() {
+        if getIntFromLabel() <= -10 {
+            buttons[2].isEnabled = false
+            callAllert()
+        } else if getIntFromLabel() >= 10 {
+            buttons[0].isEnabled = false
+            callAllert()
+        } else {
+            buttons[0].isEnabled = true
+            buttons[2].isEnabled = true
+        }
+    }
+    
 }
 
